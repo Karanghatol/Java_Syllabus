e1dc78ae287c4385ebb70d006f5f94efe66caae7
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2023-10-07 00:55:35 +0530

    Class and Object
    demonstration of class and object

---
+++ Obj.java
@@ -0,0 +1,10 @@
+
+class Obj{
+    int id = 101;
+    String name="karan ghatol";
+    public static void main(String[] args){
+        Obj o=new Obj();
+        System.out.println(o.id);
+        System.out.println(o.name);
+    }
+}
