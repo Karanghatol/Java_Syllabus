681ab30c65d4457c516f55b44a4e357ca86859b7
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2023-11-05 01:47:59 +0530

    array demonstration

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array{
+    public static void main(String[] args){
+        String a[]={"d","g","j","h","h"};
+                for(int i = 0;i<a.length;i++){
+        System.out.println(a[i]);
+        }
+    }
+}
